236be88908d7cafcf53bad38ef536aeb24b79863
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-12-09 12:59:02 +0300

    sayfa80

---
+++ sayfa80/sayfa80/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program //sayfa9* bırıncı odev
+{
+    static void Main()
+    {
+        try
+        {
+           
+            Console.Write("Kaç adet sayı gireceksiniz? ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+
+            double toplam = 0;
+
+         
+            for (int i = 0; i < adet; i++)
+            {
+                Console.Write($"{i + 1}. sayıyı giriniz: ");
+                double sayi = Convert.ToDouble(Console.ReadLine());
+                toplam += sayi;
+            }
+
+           
+            double ortalama = toplam / adet;
+
+          
+            Console.WriteLine($"Girilen sayıların toplamı: {toplam}");
+            Console.WriteLine($"Girilen sayıların ortalaması: {ortalama}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçerli bir sayı giriniz.");
+        }
+        Console.ReadKey();
+    }
+}
